754604aa0f0f5aa8fc5a3b65dd9fca0ffe57e82f
frozzway <frozz.way@gmail.com>
2024-03-22 18:17:01 +0500

    Вырезал реализацию на csharp

--- csharp/ArmGSService/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace ArmGSService.Extensions;
-
-public static class StringExtensions
-{
-    public static string ReplaceFirstCharToLowercase(this string input)
-    {
-        return !string.IsNullOrEmpty(input) ? char.ToLower(input[0]) + input[1..] : input;
-    }
-}
